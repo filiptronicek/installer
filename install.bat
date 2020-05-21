c54075227f5a231fbba354d7723bf92e5fd0bd09
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-21 14:04:40 +0200

    Add programs

--- install.bat
+++ install.bat
@@ -1,22 +1,33 @@
 @echo off  
+rem Microsoft
 call :download "Windows.WindowsTerminal"
 call :download "Microsoft.Powertoys"
 call :download "Microsoft.VisualStudioCode"
 
+rem Productivity Utils
 call :download "7zip.7zip"
 call :download "CPUID.CPU-Z"
 call :download "Balena.Etcher"
 call :download "RealVNC.VNCViewer"
+call :download "Insecure.Nmap"
+call :download "Kitware.CMake"
 
+rem Github
 call :download "Git.Git"
 call :download "Github.GitHubDesktop"
 
+rem Development Environments
 call :download "Python.Python"
 
+rem Utils
 call :download "qBittorrent.qBittorrent"
 call :download "Apple.iTunes"
 call :download "Bitwarden.Bitwarden"
+
+rem Gaming
 call :download "Valve.Steam"
+call :download "EpicGames.EpicGamesLauncher"
+call :download "Discord.Discord"
 
 REM Download function
 :download
